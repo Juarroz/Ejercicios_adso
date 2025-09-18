65f7de43bdf1b57f49eb4a1fd654d0bcc9db8ead
Fabian Sanchez <dfabiansg@gmail.com>
2025-09-18 11:04:18 -0500

    crud contacto formulario

---
+++ Fabian/urls.php
@@ -0,0 +1,6 @@
+<?php
+$BASE_URL = "http://localhost:8080/api";
+
+$URL_CONTACTOS    = $BASE_URL . "/contactos";   // para POST y GET (colección)
+$URL_CONTACTOS_ID = $BASE_URL . "/contactos/";  // para GET, PUT, DELETE (por id)
+?>
